3840da8ce4a4a2dfff9bb8b6488d4fa03872d4ce
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-06-02 18:05:07 +0700

    ManualException 11

--- ManualException.cpp
+++ ManualException.cpp
@@ -2,3 +2,12 @@
 using namespace std;
 
 int main()
+{
+    //membuat isi dari try untuk pengujian error
+    try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        //throw 0,5; //melemparkan sebuah integer  maka
+        cout << "pernyataan tidak akan di tampilkan" << endl;
+    }
+}
