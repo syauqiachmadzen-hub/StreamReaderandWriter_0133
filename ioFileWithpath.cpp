1aa8027f21b8991d5d31f7e13b0afa067b7e4708
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-06-02 17:44:55 +0700

    ioFileWithpath 6

--- ioFileWithpath.cpp
+++ ioFileWithpath.cpp
@@ -3,3 +3,14 @@
 #include <string>
 using namespace std;
 
+int main()
+{
+    //deklarasi variable baris dan NamaFile
+    string baris, NamaFile;
+
+    //membuat input untuk menangkap nama file
+    cout << "Masukkan nama file : ";
+    cin >> NamaFile;
+
+    
+}
