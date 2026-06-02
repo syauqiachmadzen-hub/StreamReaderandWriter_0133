1c5becb2a6af923559a6796f1c011119348ab2aa
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-06-02 17:35:54 +0700

    ExceptionStandart 2

--- ExceptionStandart.cpp
+++ ExceptionStandart.cpp
@@ -2,3 +2,15 @@
 #include <exception>
 #include <array>
 using namespace std;
+
+int main()
+{
+    cout << "Awal proogram" << endl; //penanda 1
+    try
+    {
+        //deklarasi array data dengan panjang index 3
+        array <int, 3> data = { 1, 2, 3 };
+        //memanggil array elemant ke-5 yang berarti akan terjadi error
+        cout << data.at(5) << endl;
+    }
+}
